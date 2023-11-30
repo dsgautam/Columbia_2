7c4019438b3b60dd2cd44bfb19fbe943d664c149
dsgautam <146027023+dsgautam@users.noreply.github.com>
2023-11-29 20:15:26 -0500

    Add files via upload

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+# My first program in Python
+print('Hello World')
+# Write your name
+x=input("What is your Name? ")
+# Print name
+print ("My name is " + x)
+print ("I can also write this as,", x)
