958a1a51a01bcc99bc4c37ebe767d89fb9e9bdad
dsgautam <146027023+dsgautam@users.noreply.github.com>
2023-11-29 21:24:16 -0500

    This is my First pull request

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 My first Git Repo
 I saved my first python file into Github.
 # Cloning was easy. I just tried it
+learning to create a brnach
---
+++ helloWorld2.py
@@ -0,0 +1,9 @@
+# My first program in Python
+print('Hello World')
+# Write your name
+x=input("What is your Name? ")
+# Print name
+print ("My name is " + x)
+print ("I can also write this as,", x)
+print ("I Cloned my repo in desktop")
+Print (" This is tge second version")
